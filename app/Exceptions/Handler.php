7247d8e0bf92958d6671dafec8d3b4d7ebef203b
Hung Truong <hungtruongquoc@gmail.com>
2025-03-22 01:22:32 -0500

    Added type hinting

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use App\Http\Exceptions\RecipeNotFoundException;
 use App\Http\Exceptions\RecipeUpdateException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
@@ -19,14 +20,14 @@ class Handler extends ExceptionHandler
 
     public function register(): void
     {
-        $this->renderable(function (RecipeNotFoundException $e, $request) {
+        $this->renderable(function (RecipeNotFoundException $e, Request $request) {
             return response()->json([
                 'error' => $e->getMessage(),
                 'code' => 'RECIPE_NOT_FOUND'
             ], $e->getCode());
         });
 
-        $this->renderable(function (IngredientNotFoundException $e, $request) {
+        $this->renderable(function (IngredientNotFoundException $e, Request $request) {
             return response()->json([
                 'error' => $e->getMessage(),
                 'code' => 'INGREDIENT_NOT_FOUND'
@@ -34,7 +35,7 @@ class Handler extends ExceptionHandler
         });
 
         // Handle RecipeCreationException
-        $this->renderable(function (RecipeCreationException $e, $request) {
+        $this->renderable(function (RecipeCreationException $e, Request $request) {
             return response()->json([
                 'error' => $e->getMessage(),
                 'code' => 'RECIPE_CREATION_FAILED'
@@ -42,7 +43,7 @@ class Handler extends ExceptionHandler
         });
 
         // Handle RecipeUpdateException
-        $this->renderable(function (RecipeUpdateException $e, $request) {
+        $this->renderable(function (RecipeUpdateException $e, Request $request) {
             return response()->json([
                 'error' => $e->getMessage(),
                 'code' => 'RECIPE_UPDATE_FAILED'
@@ -50,7 +51,7 @@ class Handler extends ExceptionHandler
         });
 
         // Handle ModelNotFoundException
-        $this->renderable(function (ModelNotFoundException $e, $request) {
+        $this->renderable(function (ModelNotFoundException $e, Request $request) {
             $modelName = strtolower(class_basename($e->getModel()));
             return response()->json([
                 'error' => "The requested {$modelName} was not found",
@@ -59,7 +60,7 @@ class Handler extends ExceptionHandler
         });
 
         // Handle ValidationException
-        $this->renderable(function (ValidationException $e, $request) {
+        $this->renderable(function (ValidationException $e, Request $request) {
             return response()->json([
                 'error' => 'The given data was invalid',
                 'code' => 'VALIDATION_ERROR',
