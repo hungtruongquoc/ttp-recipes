564dfefa9fa517518e250cb0968d407753e7aa40
Hung Truong <hungtruongquoc@gmail.com>
2025-03-21 21:01:13 -0500

    Adjusted the API routes to match the call from frontend

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/recipes', [RecipeController::class, 'getRecipes']);
-Route::get('recipe/new', [RecipeController::class, 'newRecipe']);
+Route::post('/recipes', [RecipeController::class, 'newRecipe']);
