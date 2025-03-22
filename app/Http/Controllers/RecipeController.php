14e118f8e65d733951627302048e7eef358212aa
Hung Truong <hungtruongquoc@gmail.com>
2025-03-21 21:08:22 -0500

    Adjusted the newRecipe method to get data from body and added transaction

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -6,13 +6,15 @@ use App\Models\Ingredient;
 use App\Models\Recipe;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class RecipeController extends Controller
 {
     public function getRecipes(Request $request): JsonResponse
     {
         $recipes = Recipe::get();
-    
+
         $formattedRecipes = $recipes->map(function ($recipe) {
             return [
                 'id' => $recipe->id,
@@ -26,37 +28,53 @@ class RecipeController extends Controller
                 }),
             ];
         });
-    
+
         return response()->json($formattedRecipes);
     }
 
     public function newRecipe(Request $request): JsonResponse
     {
-        $name = $request->get('name');
-        $description = $request->get('description');
-        $ingredientNames = json_decode($request->get('ingredients'));
-    
-        $recipe = new Recipe();
-        $recipe->name = $name;
-        $recipe->description = $description;
-        $recipe->save();
-    
-        foreach ($ingredientNames as $ingredientName) {
-            $ingredient = new Ingredient();
-            $ingredient->name = $ingredientName;
-            $recipe->ingredients()->save($ingredient);
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $ingredientNames = $request->input('ingredients');
+
+        // Use a transaction to ensure atomicity
+        try {
+            DB::beginTransaction();
+
+            $recipe = new Recipe();
+            $recipe->name = $name;
+            $recipe->description = $description;
+            $recipe->save();
+
+            foreach ($ingredientNames as $ingredientName) {
+                $ingredient = new Ingredient();
+                $ingredient->name = $ingredientName;
+                $recipe->ingredients()->save($ingredient);
+            }
+
+            DB::commit();
+
+            return response()->json([
+                'id' => $recipe->id,
+                'name' => $recipe->name,
+                'description' => $recipe->description,
+                'ingredients' => $recipe->ingredients->map(function ($ingredient) {
+                    return [
+                        'id' => $ingredient->id,
+                        'name' => $ingredient->name,
+                    ];
+                }),
+            ]);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            // Log the error for debugging
+            Log::error('Failed to create recipe: ' . $e->getMessage());
+
+            return response()->json([
+                'error' => 'Failed to create recipe'
+            ], 500);
         }
-    
-        return response()->json([
-            'id' => $recipe->id,
-            'name' => $recipe->name,
-            'description' => $recipe->description,
-            'ingredients' => $recipe->ingredients->map(function ($ingredient) {
-                return [
-                    'id' => $ingredient->id,
-                    'name' => $ingredient->name,
-                ];
-            }),
-        ]);
     }
 }
